f8ed4422e05caf5f3cfb866ddca8f25e94b48978
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2022-10-19 17:49:09 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,8 +6,8 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	if (n >= 0 && n <= 15)
 	
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -24,7 +24,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(k + '0');				
+					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');				
+					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
@@ -46,3 +46,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
+}
