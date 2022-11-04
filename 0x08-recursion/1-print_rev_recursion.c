22a8538fd60f7ea29935dd0c8a498af722dbb212
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2022-11-05 01:35:37 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to reverse a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
